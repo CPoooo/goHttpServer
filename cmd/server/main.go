811b83cfb67f77c0e5cda56343c2f2ac0be13b42
CPoooo <cameronpool2019@gmail.com>
2026-03-22 10:53:01 -0500

    TCP listener created, listening and printing out any connections

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,12 +1,18 @@
 package main
 
 // just fyw InitDb() in db.go does not need to be imported like a module in js land
+// know how import works and how go looks for/uses packages
 import (
 	"fmt"
+	"net"
 	"os"
 
 	"github.com/CPoooo/goHttpServer/internal/db"
 )
+
+func handleConnection(conn net.Conn) {
+	fmt.Printf("New connection received: %v\n", conn)
+}
 
 func main() {
 	// conn here is a dbpool - name it that?
@@ -15,7 +21,34 @@ func main() {
 		fmt.Fprintf(os.Stderr, "Failed to connect to DB: %v\n", err)
 		os.Exit(1)
 	}
-	defer dbpool.Close()
 	// do not defer the closing of the db connection from inside the init to the db! (duh)
-	// defer dbpool.Close()
-} // defer running btw -- closing connection conn.Close
+	defer dbpool.Close()
+
+	// open tcp
+	listener, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Failed to create TCP Listener: %v\n", err)
+		os.Exit(1)
+	}
+	defer listener.Close()
+	for {
+		// Read bytes from tcp stream
+		connection, err := listener.Accept()
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Failed to Accept() on TCP Listener: %v\n", err)
+			os.Exit(1)
+		}
+		go handleConnection(connection)
+	}
+
+	// get request
+
+	// print request
+
+	// parse and marshal/unmarshal to and from json - how to know if the bytes from req
+	// that i received are http vs another protocol
+
+	// marshal into json and send back over tcp to client
+	// each connection needs a goroutine handler to be run
+
+}
